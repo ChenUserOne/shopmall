b96f458565e2b1d62a8a802c4f6bc8243da30471
hu <1771080823@qq.com>
2022-07-03 00:26:28 +0800

    项目初始结构创建

---
+++ shopmall-coupon/shopmall-coupon/src/test/java/com/itcast/shopmall/coupon/ShopmallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itcast.shopmall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopmallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ shopmall-member/shopmall-member/src/main/java/com/itcast/shopmall/member/ShopmallMemberApplication.java
@@ -0,0 +1,13 @@
+package com.itcast.shopmall.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopmallMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopmallMemberApplication.class, args);
+    }
+
+}
